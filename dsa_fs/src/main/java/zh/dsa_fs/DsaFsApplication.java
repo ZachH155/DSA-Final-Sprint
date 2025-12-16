58d99a470bae9d54e77e620cc87aa1e8ab535879
ZachHulan155 <zachary.hulan@keyin.com>
2025-12-16 00:38:26 -0330

    Added SB Maven Java

---
+++ dsa_fs/src/main/java/zh/dsa_fs/DsaFsApplication.java
@@ -0,0 +1,13 @@
+package zh.dsa_fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaFsApplication.class, args);
+	}
+
+}
---
+++ dsa_fs/src/test/java/zh/dsa_fs/DsaFsApplicationTests.java
@@ -0,0 +1,13 @@
+package zh.dsa_fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
