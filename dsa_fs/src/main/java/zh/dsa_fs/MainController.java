72682370b5aedfbf9ff889e0b7689127a6a7082c
ZachHulan155 <zachary.hulan@keyin.com>
2025-12-17 04:09:30 -0330

    completed enter-numbers page

--- dsa_fs/src/main/java/zh/dsa_fs/MainController.java
+++ dsa_fs/src/main/java/zh/dsa_fs/MainController.java
@@ -1,9 +1,16 @@
 package zh.dsa_fs;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -16,10 +23,33 @@ public class MainController {
     }
     
 
+    //enter numbers page
     @RequestMapping("/enter-numbers")
-    public String EnterNumbers(Model model) {
-        model.addAttribute("style", "../css/styles.css");
-
+    public String renderEnterNumbers() {
         return "enter-numbers";
     }
+
+    @PostMapping("/enter-numbers")
+    public String postEnterNumbers(@RequestParam("numbers") String numbers) { 
+        String[] numbersArray = null;
+        numbersArray = numbers.split(",");
+        return "redirect:/enter-numbers";
+    }
+
+    @PostMapping("/past-trees")
+    public String redirectPreviousTrees() {
+        return "redirect:/previous-trees";
+    }
+
+
+    //previous trees page
+    @RequestMapping("/previous-trees")
+    public String renderPreviousTrees() {
+        return "previous-trees";
+    }
+    
+    
+    
 }
+    
+
